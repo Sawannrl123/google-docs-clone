12e654214a82daf08b320ed8b654fa9dcbfd0ead
Sawan Kumar <sawankumar@apollo247.org>
2020-03-25 19:19:54 +0530

    Updates

---
+++ precache-manifest.ffc6d03f6c5347bc06858830b0469795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ba5937d18a7b9cf1f4558cd63b918c",
+    "url": "/google-docs-clone/index.html"
+  },
+  {
+    "revision": "5f4c9812e636dabc7c5d",
+    "url": "/google-docs-clone/static/css/main.10423165.chunk.css"
+  },
+  {
+    "revision": "381af9b633a1982f8861",
+    "url": "/google-docs-clone/static/js/2.47999e65.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/google-docs-clone/static/js/2.47999e65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4c9812e636dabc7c5d",
+    "url": "/google-docs-clone/static/js/main.fdc3b1d0.chunk.js"
+  },
+  {
+    "revision": "aed8826db0f79d7e6ca6",
+    "url": "/google-docs-clone/static/js/runtime-main.e8883142.js"
+  }
+]);
